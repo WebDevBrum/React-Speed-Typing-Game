f836a8a3e3cb189b4b147f29e0a21542c295b5b8
Scott Davies <scottdavies1983@gmail.com>
2021-04-30 10:46:09 +0100

    word count function

--- speed-typing/App.js
+++ speed-typing/App.js
@@ -1,22 +1,40 @@
 import React, {useState} from "react"
 
 /**
- * Challenge: Using hooks, track the state of the text in the textarea on every keystroke
- * To verify it's working, you could just console.log the state on every change
+ * Challenge:
  * 
- * https://scrimba.com/p/p7P5Hd/cW8Jdfy
+ * Create a function to calculate the number of separate words in the `text` state
+ * For now, just console.log the word count when the button gets clicked to test it out.
  */
 
 function App() {
-    const [text, setText] = useState("text here");
-    console.log(text);
+    const [text, setText] = useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    function wordCount(str) {
+        // str = str.replace(/(^\s*)|(\s*$)/gi,""); regex expression will remove the start and end spaces of the given string.
+        //  str = str.replace(/[ ]{2,}/gi," "); multiple spaces to a single space.
+        //  str = str.replace(/\n /,"\n"); exclude a new line with a start spacing.
+        //  console.log(str.split(' ').length);
+        //  return str.split(' ').length;
+        const wordsArr = text.trim().split(" ")
+        console.log(wordsArr.filter(word => word !== "").length);
+        return wordsArr.filter(word => word !== "").length;
+    }
     
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea value={text} onChange={(e) => setText(e.target.value)}/>
-            <h4>Time remainaing: ???</h4>
-            <button>Start</button>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: ???</h4>
+            <button onClick={() => wordCount(text)} >Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
