7aeb44b633cd130b0abbe81670c1577a1539a31f
Scott Davies <scottdavies1983@gmail.com>
2021-08-12 20:01:57 +0100

    my solution to , After the game ends, make it so the user can click the Start button again  * to play a second time

--- speed-typing/App.js
+++ speed-typing/App.js
@@ -5,11 +5,14 @@ import React, {useState, useEffect} from "react"
  * 
  * When the timer reaches 0, count the number of words the user typed in 
  * and display it in the "Word count" section
+ * 
+ * After the game ends, make it so the user can click the Start button again
+ * to play a second time
  */
 
 function App() {
     const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(2)
+    const [timeRemaining, setTimeRemaining] = useState(5)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
     const [wordCount, setWordCount] = useState(0)
     
@@ -21,6 +24,13 @@ function App() {
     function calculateWordCount(text) {
         const wordsArr = text.trim().split(" ")
         return wordsArr.filter(word => word !== "").length
+    }
+    
+    function startGame() {
+        setTimeRemaining(5);
+        setText("");
+        setWordCount(0);
+        setIsTimeRunning(true);
     }
     
     useEffect(() => {
@@ -42,10 +52,11 @@ function App() {
                 value={text}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => setIsTimeRunning(true)}>Start</button>
+            <button onClick={() => startGame()}>Start</button>
             <h1>Word count: {wordCount}</h1>
         </div>
     )
 }
 
 export default App
+
