a43319e3ed2fcffcc39eceaacf29fcba0e93704b
Scott Davies <scottdavies1983@gmail.com>
2021-08-23 05:55:47 +0100

    added useRef hook for focus on text area at application start

--- speed-typing/App.js
+++ speed-typing/App.js
@@ -1,23 +1,20 @@
-import React, {useState, useEffect} from "react"
+mport React, {useState, useEffect, useRef} from "react"
 
 /**
  * Challenge:
  * 
- * When the timer reaches 0, count the number of words the user typed in 
- * and display it in the "Word count" section
- * 
- * After the game ends, make it so the user can click the Start button again
- * to play a second time
+ * Make the input box focus (DOM elements have a method called .focus()) 
+ * immediately when the game starts
  */
 
 function App() {
-    const STARTING_TIME = 15
+    const STARTING_TIME = 5
     
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
     const [wordCount, setWordCount] = useState(0)
-    const [disable, setDisable] = useState(false);
+    const textBoxRef = useRef(null)
     
     function handleChange(e) {
         const {value} = e.target
@@ -33,16 +30,14 @@ function App() {
         setIsTimeRunning(true)
         setTimeRemaining(STARTING_TIME)
         setText("")
-        setDisable(true)
+        textBoxRef.current.disabled = false
+        textBoxRef.current.focus()
     }
     
     function endGame() {
         setIsTimeRunning(false)
         setWordCount(calculateWordCount(text))
-        setDisable(false)
     }
-    
-    // https://www.google.com/search?q=Disable+button+in+react
     
     useEffect(() => {
         if(isTimeRunning && timeRemaining > 0) {
@@ -58,11 +53,18 @@ function App() {
         <div>
             <h1>How fast do you type?</h1>
             <textarea
+                ref={textBoxRef}
                 onChange={handleChange}
                 value={text}
+                disabled={!isTimeRunning}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button disabled={disable} onClick={startGame}>Start</button>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
             <h1>Word count: {wordCount}</h1>
         </div>
     )
